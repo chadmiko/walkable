f7ed29c8f3405a719bd533885c01e0bca4c473e8
Chris Butera <chrisbutera@gmail.com>
2012-02-04 14:55:24 -0600

    Working Groupon query.

--- java/ApiCall.java
+++ java/ApiCall.java
@@ -13,11 +13,12 @@
  */
 public class ApiCall {
 
-	static final String consumerKey = "j0sM9V8eFDcYn0PmBTdqTA";
-	static final String consumerSecret = "ihXDy_ikwu8DFZEjk4PiK754Y7k"; 
-	static final String token = "rw2ExtIjLfYk-BAp2mF-wUwh3-4IUGKi";
-	static final String tokenSecret = "5yNUcFAF53iGqkD7ROwHskqm2ys";
+	static final String yConsumerKey = "j0sM9V8eFDcYn0PmBTdqTA";
+	static final String yConsumerSecret = "ihXDy_ikwu8DFZEjk4PiK754Y7k"; 
+	static final String yToken = "rw2ExtIjLfYk-BAp2mF-wUwh3-4IUGKi";
+	static final String yTokenSecret = "5yNUcFAF53iGqkD7ROwHskqm2ys";
 	
+	static final String gClientID = "8ad4ef59d1e755157121d9ac5f3ff16aeb89d93d";
 	
 	/**
 	 * @param args
@@ -25,12 +26,19 @@ public class ApiCall {
 	public static void main(String[] args) {
 		double lat = 41.9485056;
 		double lon = -87.6616156;
-		Yelp yelp = new Yelp(consumerKey, consumerSecret, token ,tokenSecret);
-		String response;
-//		response = yelp.search("mexican", lat, lon);
-		response = yelp.getBusiness("uncommon-ground-chicago");
-		System.out.println(response);
 
+		//Yelp Example
+//		Yelp yelp = new Yelp(yConsumerKey, yConsumerSecret, yToken ,yTokenSecret);
+//		String response;
+//		response = yelp.search("mexican", lat, lon);
+//		response = yelp.getBusiness("uncommon-ground-chicago");
+//		System.out.println(response);
+
+		//Groupon Example
+		Groupon groupon = new Groupon(gClientID);
+		String gResponse = groupon.search("chicago");
+		System.out.println(gResponse);
+		
 	}
 
 }
